91bdf2aa503f0d3c17dfa4968e214f0697fc94e0
carles <hello@carlespibernat.com>
2018-09-30 12:06:22 +0200

    Remove ApiTestCase bundle. Current version is not working with PHPUnit last version

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
-    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
