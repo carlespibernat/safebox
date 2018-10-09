5ac94fb22ed0290e6373ca9f9dd52d67e151a756
carles <hello@carlespibernat.com>
2018-10-09 20:17:34 +0200

    Create token is valid unit tests

---
+++ tests/TokenTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Token;
+use PHPUnit\Framework\TestCase;
+
+class TokenTest extends TestCase
+{
+    public function testTokenIsValid()
+    {
+        $token = new Token();
+        $token->setToken('token.test');
+        $token->setExpirationTime(new \DateTime('tomorrow'));
+
+        $this->assertTrue($token->isValid('token.test'));
+        $this->assertFalse($token->isValid('other.token'));
+
+        $token->setExpirationTime(new \DateTime('yesterday'));
+
+        $this->assertFalse('token.test');
+
+    }
+}
